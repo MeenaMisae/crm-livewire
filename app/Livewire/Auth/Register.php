00bc91b8e6898fd1513714657b3f83006616c0d3
MeenaMisae <meenahiwatashi@gmail.com>
2024-07-22 10:59:51 -0300

    feat: classe livewire de registro

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -2,12 +2,28 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\View\View;
 use Livewire\Component;
 
 class Register extends Component
 {
-    public function render()
+    public ?string $name;
+    public ?string $email;
+    public ?string $password;
+    public ?string $email_confirmation;
+
+    public function render(): View
     {
         return view('livewire.auth.register');
+    }
+
+    public function submit(): void
+    {
+        User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => $this->password
+        ]);
     }
 }
