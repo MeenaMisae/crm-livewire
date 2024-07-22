d3fb35c5a124ab70aa200041d760957c34eb7d41
MeenaMisae <meenahiwatashi@gmail.com>
2024-07-22 11:00:59 -0300

    test: teste de registro de usuário

--- tests/Feature/Livewire/Auth/RegisterTest.php
+++ tests/Feature/Livewire/Auth/RegisterTest.php
@@ -5,5 +5,21 @@ use Livewire\Livewire;
 
 it('should render the component', function () {
     Livewire::test(Register::class)
-    ->assertOk();
+        ->assertOk();
+});
+
+it('should be able to register a new user', function () {
+    Livewire::test(Register::class)
+        ->set('name', 'Test User')
+        ->set('email', 'test@example.com')
+        ->set('password', 'password')
+        ->call('submit')
+        ->assertHasNoErrors();
+
+    \Pest\Laravel\assertDatabaseHas('users', [
+        'name' => 'Test User',
+        'email' => 'test@example.com'
+    ]);
+
+    \Pest\Laravel\assertDatabaseCount('users', 1);
 });
