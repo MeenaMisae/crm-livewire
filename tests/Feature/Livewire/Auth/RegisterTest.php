93514d6c46efb549633194913a30f301fbff5463
MeenaMisae <meenahiwatashi@gmail.com>
2024-07-24 14:15:28 -0300

    test: adiciona teste de registro e redirecionamento

--- tests/Feature/Livewire/Auth/RegisterTest.php
+++ tests/Feature/Livewire/Auth/RegisterTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\Auth\Register;
+use App\Models\User;
 use Livewire\Livewire;
 
 it('should render the component', function () {
@@ -14,7 +15,8 @@ it('should be able to register a new user', function () {
         ->set('email', 'test@example.com')
         ->set('password', 'password')
         ->call('submit')
-        ->assertHasNoErrors();
+        ->assertHasNoErrors()
+        ->assertRedirect(route('home'));
 
     \Pest\Laravel\assertDatabaseHas('users', [
         'name' => 'Test User',
@@ -22,6 +24,8 @@ it('should be able to register a new user', function () {
     ]);
 
     \Pest\Laravel\assertDatabaseCount('users', 1);
+
+    expect(auth()->check())->and(auth()->user())->id->toBe(User::first()->id);
 });
 
 test('validation rules', function ($data) {
